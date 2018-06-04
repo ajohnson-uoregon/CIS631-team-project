b4031441dcea6831a8889f940f8de1f6b09993e7
Erin McCarthy <mccarthyern@gmail.com>
2018-06-03 22:06:38 -0700

    made some changes

--- recommend.cpp
+++ recommend.cpp
@@ -1,6 +1,7 @@
 #include "recommend.h"
 #include<iostream>
 #include <algorithm>
+#include <accumulate>
 // #include <cuda_runtime.h>
 // #include "cublas_v2.h"
 // #define M 6
@@ -10,6 +11,7 @@
 //add to the header file
 std::vector<double> multHelp(std::vector<double> vect1, std::vector<double> vect2) {
   std::vector<double> output;
+  // foreach maybe replace
   std::transform(vect1.begin(), vect1.end(), vect2.begin(), output.begin(), std::multiplies<double>()); 
   return output;
 }
@@ -32,13 +34,30 @@ std::list<int> recommend(int userid, std::vector<std::vector<int>> user_items,
   // for vector in item_factors call mult help
  
   // std::transform(item_factors.begin(), item_factors.end(), , std::bind1st(std::multiplies<T>(),3));
+  for (auto iter = item_factors.begin(); iter != item_factors.end(); ++iter){
+    scores.push_back(multHelp(&iter, user))
+  }
+
+  // idk what scores_np = scores.getArray() does so I am ignoring it for now
+  // transform on outer and accumulate on inner to sum score
   int count = n + liked.size();
   
-  std::vector<double> scores_np;
+  std::vector<std::vector<double>> scores_np;
   // check if it should be < or <=
   for (int i =0; i <scores.size(); i++) {
-  
-  };
+    currScore = scores[i];
+    for(auto iterL = liked.begin(); iterL != liked.end(); ++iterL) {
+      if(currScore == &iterL)
+      {
+        scores_np.push_back(currScore);
+      }
+    }
+  }
+
+
+
+
+
   //std::cout << users << std::endl;
   std::list<int> ans = {3};
   
