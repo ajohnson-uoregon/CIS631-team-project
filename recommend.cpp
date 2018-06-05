271d0f50eb64acaaf44cb430458e7f7130a97cec
Erin McCarthy <mccarthyern@gmail.com>
2018-06-05 15:28:52 -0700

    finished the code from python and compiles, need to fix return statement

--- recommend.cpp
+++ recommend.cpp
@@ -1,7 +1,8 @@
 #include "recommend.h"
 #include<iostream>
 #include <algorithm>
-#include <accumulate>
+#include <numeric>
+#include<tuple>
 // #include <cuda_runtime.h>
 // #include "cublas_v2.h"
 // #define M 6
@@ -35,30 +36,34 @@ std::list<int> recommend(int userid, std::vector<std::vector<int>> user_items,
  
   // std::transform(item_factors.begin(), item_factors.end(), , std::bind1st(std::multiplies<T>(),3));
   for (auto iter = item_factors.begin(); iter != item_factors.end(); ++iter){
-    scores.push_back(multHelp(&iter, user))
+    scores.push_back(multHelp(*iter, user));
   }
 
-  // idk what scores_np = scores.getArray() does so I am ignoring it for now
-  // transform on outer and accumulate on inner to sum score
+  // score should be a vector 
+  std::vector<double> score_vec = *(scores.begin());
+  for (auto scoreI = ++(scores.begin()); scoreI != scores.end(); ++scoreI) {
+    std::transform((*scoreI).begin(), (*scoreI).end(), score_vec.begin(), score_vec.begin(), std::plus<double>());
+  } 
+  // this isnt used
   int count = n + liked.size();
   
-  std::vector<std::vector<double>> scores_np;
+  std::vector<std::tuple<int,double>> scores_np;
+  
   // check if it should be < or <=
-  for (int i =0; i <scores.size(); i++) {
-    currScore = scores[i];
+  for (int i =0; i <score_vec.size(); ++i) {
+    double currScore = score_vec[i];
     for(auto iterL = liked.begin(); iterL != liked.end(); ++iterL) {
-      if(currScore == &iterL)
+      if(currScore == *iterL)
       {
-        scores_np.push_back(currScore);
+        scores_np.push_back(std::tuple<int, double>(i, currScore));
       }
     }
   }
-
-
-
-
-
+  // now sort by the second thing in the list
   //std::cout << users << std::endl;
+  std::sort(scores_np.begin(), scores_np.end(), [](std::tuple<int,double> const &t1, std::tuple<int,double> const &t2) {
+      return std::get<1>(t1) > std::get<1>(t2);
+    });
   std::list<int> ans = {3};
   
   return ans;
