e5a4fc04ada3663d3c7391788ba51742a9398764
Erin McCarthy <mccarthyern@gmail.com>
2018-06-10 16:34:15 -0700

    updating comments

--- recommend.cpp
+++ recommend.cpp
@@ -1,15 +1,8 @@
 #include "recommend.h"
 
-// #include <cuda_runtime.h>
-// #include "cublas_v2.h"
-// #define M 6
-// #define N 5
-// #define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1)) 
 
-//add to the header file
 std::vector<double> multHelp(std::vector<double> vect1, std::vector<double> vect2) {
   std::vector<double> output;
-  // foreach maybe replace
   std::transform(vect1.begin(), vect1.end(), vect2.begin(), output.begin(), std::multiplies<double>()); 
   return output;
 }
@@ -20,18 +13,12 @@ std::vector<std::tuple<int,double>> recommend(int userid, std::vector<std::vecto
 			     			      int n)
 {
   int users = user_items.size(), items = user_items[0].size(), factors = user_items[0].size();
-  // idk what type vector is so double is just a filler
   std::vector<double> user;
   user = user_factors[userid];  
-  //this is possibly just ints but idk
-  // index into vector by userid [0]
+
   std::vector<int> liked = user_items[userid];
   std::vector<std::vector<double>> scores;
-  // what is item factors??
-  // item_factors is a matrix * user vector
-  // for vector in item_factors call mult help
- 
-  // std::transform(item_factors.begin(), item_factors.end(), , std::bind1st(std::multiplies<T>(),3));
+  
   for (auto iter = item_factors.begin(); iter != item_factors.end(); ++iter){
     scores.push_back(multHelp(*iter, user));
   }
@@ -41,7 +28,7 @@ std::vector<std::tuple<int,double>> recommend(int userid, std::vector<std::vecto
   for (auto scoreI = ++(scores.begin()); scoreI != scores.end(); ++scoreI) {
     std::transform((*scoreI).begin(), (*scoreI).end(), score_vec.begin(), score_vec.begin(), std::plus<double>());
   } 
-  // this isnt used
+  // this isnt used but it is the python code
   int count = n + liked.size();
   
   std::vector<std::tuple<int,double>> scores_np;
