42f7ea5104e389a24854159aef48296d8fbf055f
Alister Johnson <ajohnson@cs.uoregon.edu>
2018-06-10 20:46:51 -0700

    least squares lives

--- leastsqr.cu
+++ leastsqr.cu
@@ -1,5 +1,9 @@
 #include <cstring>
-#include <algoithm>
+#include <algorithm>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#include <cusolverDn.h>
+
 double** least_squares (int* indptr, int* indic, double* data, int users, 
                         int items, int factors,
                         double** x, double** y, 
@@ -7,9 +11,11 @@ double** least_squares (int* indptr, int* indic, double* data, int users,
 {
     cublasStatus_t err;
     cublasHandle_t handle;
+    cusolverDnHandle_t solve_handle;
     const double alpha = 1;
     const double beta = 0;
-    double[factors][items] yt;
+    double yt[factors][items];
+    
     err = cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, items, factors,
         &alpha, y[0], items, &beta, y[0], items, yt[0], factors);
     
@@ -18,7 +24,7 @@ double** least_squares (int* indptr, int* indic, double* data, int users,
     for (int i =0; i < users; ++i) {
         
         int rowStart = indptr[i];
-        int rowEnd = intptr[i+1];
+        int rowEnd = indptr[i+1];
 
         int cols[rowEnd-rowStart];
         memcpy(cols, &indic[rowStart], rowEnd-rowStart);
@@ -26,34 +32,35 @@ double** least_squares (int* indptr, int* indic, double* data, int users,
         double vals[rowEnd-rowStart];
         memcpy(vals, &data[rowStart], rowEnd-rowStart);
 
-        double[items] diagVec;
-        fill(diagVec.start(), diagVec.end(), 0);
+        double diagVec[items];
+        std::fill_n(diagVec, items, 0);
 
         for(int iter = 0; iter < (rowEnd-rowStart); ++iter) 
         {
             diagVec[cols[iter]] = vals[iter];
         }   
-        double[items][factors] ansC;
+        double ansC[items][factors];
 
-        cublasDdgmm(handle, mode, items, factors, y[0], items, diagVec, 1, ansC, items);
-        double[factors][factors] ytcy;
+        cublasDdgmm(handle, mode, items, factors, y[0], items, diagVec, 1, ansC[0], items);
+        double ytcy[factors][factors];
 
         cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, factors, factors, 
-                        items, &alpha, yt, factors, ansC, items, &beta, ytcy, factors);
-        for (int i = 0; i < factors; ++i) 
+                        items, &alpha, yt[0], factors, ansC[0], items, &beta, ytcy[0], factors);
+        for (int k = 0; k < factors; ++k) 
         {
-            ytcy[i][i] += reg;
+            ytcy[k][k] += reg;
         }
 
-        double[factors] ytcu;
-        cublasDgemv (handle, CUBLAS_OP_N, CUBLAS_OP_N, factors, items,
-                        &alpha, yt, factors, diagVec, 1, &beta, ytcu, 1);
-        int* Lwork;
-        cusolverDnDgetrf_bufferSize(handle, factors, factors, ytcy, facotrs, Lwork);
+        double ytcu[factors];
+        cublasDgemv (handle, CUBLAS_OP_N, factors, items,
+                        &alpha, yt[0], factors, diagVec, 1, &beta, ytcu, 1);
+        int Lwork;
+        cusolverDnDgetrf_bufferSize(solve_handle, factors, factors, ytcy[0], factors, &Lwork);
         int* rfOut;
-        cusolverDnDgetrf(handle, factors, factors, ytcy, factors, Lwork, NULL, rfOut);
+        double workspace[Lwork];
+        cusolverDnDgetrf(solve_handle, factors, factors, ytcy[0], factors, workspace, NULL, rfOut);
         int* rsOut;
-        cusolverDnDgetrs(handle, CUBLAS_OP_N, factors, 1, ytcy, factors, NULL, ytcu, factors, rsOut);
+        cusolverDnDgetrs(solve_handle, CUBLAS_OP_N, factors, 1, ytcy[0], factors, NULL, ytcu, factors, rsOut);
         x[i] = ytcu;
     }
 
