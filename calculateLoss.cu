b102cd2674650b6c76314581f0cf2187f1ec252a
Alister Johnson <ajohnson@cs.uoregon.edu>
2018-06-12 23:46:31 -0700

    comments about errors to fix

--- calculateLoss.cu
+++ calculateLoss.cu
@@ -9,6 +9,7 @@ double calculate_loss(cublasHandle_t handle, int* indptr, int* indices, double* 
       int user_norm = 0;
       cublasStatus_t err;
 
+      // malloc this
       double** YtY;
       const double alpha = 1;
       const double beta = 0;
@@ -17,7 +18,9 @@ double calculate_loss(cublasHandle_t handle, int* indptr, int* indices, double* 
       err = cublasDgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, items, factors,
                 &alpha, Y, items, &beta, Y, items, YtY[0], factors);
 
+
       for (int u = 0; u < users; ++u) {
+          cudaDeviceSynchronize();
           double temp = 1.0;
 
           double* r;
@@ -26,6 +29,7 @@ double calculate_loss(cublasHandle_t handle, int* indptr, int* indices, double* 
 
           err = cublasDgemv(handle, CUBLAS_OP_N, items, factors,
                     &alpha, Y, items, Xu, 1, &beta, r, 1);
+          cudaDeviceSynchronize();
 
           int rowStart = indptr[u];
           int rowEnd = indptr[u+1];
@@ -42,26 +46,27 @@ double calculate_loss(cublasHandle_t handle, int* indptr, int* indices, double* 
               double confidence = vals[index];
 
               double* Yi = &Y[i*factors];
-
+              cudaDeviceSynchronize();
               double d;
               err = cublasDdot(handle, factors, Yi, 1, Xu, 1, &d);
               temp = (confidence - 1)*d - (2*confidence);
-
+              cudaDeviceSynchronize();
               err = cublasDaxpy(handle, factors, &temp, Yi, 1, r, 1);
               total_confidence += confidence;
               loss += confidence;
           }
 
           double other_temp;
-
+          cudaDeviceSynchronize();
           err = cublasDdot(handle, factors, r, 1, Xu, 1, &other_temp);
           loss += other_temp;
-
+          cudaDeviceSynchronize();
           err = cublasDdot(handle, factors, Xu, 1, Xu, 1, &other_temp);
           user_norm += other_temp;
       }
 
       for (int i = 0; i < items; ++i) {
+          cudaDeviceSynchronize();
           double* Yi = &Y[i*factors];
 
           double other_temp;
--- rmse.cu
+++ rmse.cu
@@ -1,19 +1,20 @@
 #include "rmse.h"
 // #define M 6
 // #define N 5
-// #define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1)) 
+// #define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1))
 
 //Cui will look like this:
 //  Cui[0] = indptr - pointer into indices/data showing where each row starts
 //  Cui[1] = indices - which columns in that row exist
 //  Cui[2] = data - what's in that column
 
-double rmse(cublasHandle_t handle, double* user_factors, double* item_factors, 
+double rmse(cublasHandle_t handle, double* user_factors, double* item_factors,
             int* rows, int* cols, double* ratings, int num_things, int factors) {
     double error = 0;
     cublasStatus_t err;
 
     for (int k = 0; k < num_things; ++k) {
+        cudaDeviceSynchronize();
         int uid = rows[k];
         int iid = cols[k];
         double rating = ratings[k];
